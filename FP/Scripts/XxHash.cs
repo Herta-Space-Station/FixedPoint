df7360c1e08804d65671d5f8f2179ef9b1adfdf6
Molth Nevin <qq2201591870@163.com>
2025-06-12 20:29:15 +0800

    Update XxHash.cs

--- FP/Scripts/XxHash.cs
+++ FP/Scripts/XxHash.cs
@@ -101,5 +101,84 @@ namespace Herta
             return num15 ^ (int)((uint)num15 >> 16);
 #endif
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static long Hash64(ReadOnlySpan<byte> buffer, ulong seed = 0)
+        {
+            ref byte local1 = ref MemoryMarshal.GetReference(buffer);
+            int length = buffer.Length;
+            ulong num1;
+            if (buffer.Length >= 32)
+            {
+                ulong num2 = seed + 6983438078262162902UL;
+                ulong num3 = seed + 14029467366897019727UL;
+                ulong num4 = seed;
+                ulong num5 = seed - 11400714785074694791UL;
+                for (; length >= 32; length -= 32)
+                {
+                    ref byte local2 = ref Unsafe.AddByteOffset(ref local1, (IntPtr)(buffer.Length - length));
+                    ulong num6 = num2 + Unsafe.ReadUnaligned<ulong>(ref local2) * 14029467366897019727UL;
+                    num2 = (ulong)((((long)num6 << 31) | (long)(num6 >> 33)) * -7046029288634856825L);
+                    ulong num7 = num3 + Unsafe.ReadUnaligned<ulong>(ref Unsafe.AddByteOffset(ref local2, new IntPtr(8U))) * 14029467366897019727UL;
+                    num3 = (ulong)((((long)num7 << 31) | (long)(num7 >> 33)) * -7046029288634856825L);
+                    ulong num8 = num4 + Unsafe.ReadUnaligned<ulong>(ref Unsafe.AddByteOffset(ref local2, new IntPtr(16U))) * 14029467366897019727UL;
+                    num4 = (ulong)((((long)num8 << 31) | (long)(num8 >> 33)) * -7046029288634856825L);
+                    ulong num9 = num5 + Unsafe.ReadUnaligned<ulong>(ref Unsafe.AddByteOffset(ref local2, new IntPtr(24U))) * 14029467366897019727UL;
+                    num5 = (ulong)((((long)num9 << 31) | (long)(num9 >> 33)) * -7046029288634856825L);
+                }
+
+                long num10 = (((long)num2 << 1) | (long)(num2 >> 63)) + (((long)num3 << 7) | (long)(num3 >> 57)) + (((long)num4 << 12) | (long)(num4 >> 52)) + (((long)num5 << 18) | (long)(num5 >> 46));
+                ulong num11 = num2 * 14029467366897019727UL;
+                long num12 = (((long)num11 << 31) | (long)(num11 >> 33)) * -7046029288634856825L;
+                long num13 = (num10 ^ num12) * -7046029288634856825L + -8796714831421723037L;
+                ulong num14 = num3 * 14029467366897019727UL;
+                long num15 = (((long)num14 << 31) | (long)(num14 >> 33)) * -7046029288634856825L;
+                long num16 = (num13 ^ num15) * -7046029288634856825L + -8796714831421723037L;
+                ulong num17 = num4 * 14029467366897019727UL;
+                long num18 = (((long)num17 << 31) | (long)(num17 >> 33)) * -7046029288634856825L;
+                long num19 = (num16 ^ num18) * -7046029288634856825L + -8796714831421723037L;
+                ulong num20 = num5 * 14029467366897019727UL;
+                long num21 = (((long)num20 << 31) | (long)(num20 >> 33)) * -7046029288634856825L;
+                num1 = (ulong)((num19 ^ num21) * -7046029288634856825L + -8796714831421723037L);
+            }
+            else
+                num1 = seed + 2870177450012600261UL;
+
+            ulong num22 = num1 + (ulong)buffer.Length;
+            for (; length >= 8; length -= 8)
+            {
+                ulong num23 = Unsafe.ReadUnaligned<ulong>(ref Unsafe.AddByteOffset(ref local1, (IntPtr)(buffer.Length - length))) * 14029467366897019727UL;
+                ulong num24 = (ulong)((((long)num23 << 31) | (long)(num23 >> 33)) * -7046029288634856825L);
+                ulong num25 = num22 ^ num24;
+                num22 = (ulong)((((long)num25 << 27) | (long)(num25 >> 37)) * -7046029288634856825L + -8796714831421723037L);
+            }
+
+            if (length >= 4)
+            {
+                ulong num26 = Unsafe.ReadUnaligned<uint>(ref Unsafe.AddByteOffset(ref local1, (IntPtr)(buffer.Length - length)));
+                ulong num27 = num22 ^ (num26 * 11400714785074694791UL);
+                num22 = (ulong)((((long)num27 << 23) | (long)(num27 >> 41)) * -4417276706812531889L + 1609587929392839161L);
+                length -= 4;
+            }
+
+            for (int byteOffset = 0; byteOffset < length; ++byteOffset)
+            {
+                ulong num28 = Unsafe.AddByteOffset(ref Unsafe.AddByteOffset(ref local1, (IntPtr)(buffer.Length - length)), (IntPtr)byteOffset);
+                ulong num29 = num22 ^ (num28 * 2870177450012600261UL);
+                num22 = (ulong)((((long)num29 << 11) | (long)(num29 >> 53)) * -7046029288634856825L);
+            }
+
+#if NET7_0_OR_GREATER
+            long num30 = (long)num22;
+            long num31 = (num30 ^ (num30 >>> 33)) * -4417276706812531889L;
+            long num32 = (num31 ^ (num31 >>> 29)) * 1609587929392839161L;
+            return num32 ^ (num32 >>> 32);
+#else
+            long num30 = (long)num22;
+            long num31 = (num30 ^ (long)((ulong)num30 >> 33)) * -4417276706812531889L;
+            long num32 = (num31 ^ (long)((ulong)num31 >> 29)) * 1609587929392839161L;
+            return num32 ^ (long)((ulong)num32 >> 32);
+#endif
+        }
     }
 }
